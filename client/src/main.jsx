44d910fa30ff12d66a65efe38b6af33d21579ae3
joel-roshan <joeljosephroshan@gmail.com>
2023-06-16 23:17:18 +0530

    routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import Landing from "./Pages/Landing/Landing";
 import Complain from "./Pages/complain.jsx";
 import Complainform from "./Pages/complainform/Complainform";
+import VotingPage from "./Pages/VotingPage/VotingPage.jsx"
 import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,8 +13,14 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThirdwebProvider desiredChainId={ChainId.Goerli}>
     <BrowserRouter>
+      <Routes>
+      <Route path="/" Component={Landing}/>
+      <Route path="/complain" Component={Complain}/>
+      <Route path="/complainform" Component={Complainform}/>
+      <Route path="/vote" Component={VotingPage}/>
+    
       
-      <Complainform />
+      </Routes>
     </BrowserRouter>
   </ThirdwebProvider>
 );
