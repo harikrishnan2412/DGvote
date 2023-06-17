a6c0c6cf34c27ad3d0e87ee8d2a5542422eb1ef8
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2023-06-17 08:16:02 +0530

    firebase-image

--- client/src/Pages/complainform/Complainform.jsx
+++ client/src/Pages/complainform/Complainform.jsx
@@ -9,6 +9,7 @@ function Complainform() {
     title: '',
     location: '',
     desc: '',
+    photoUrl: ''
   });
 
   const handleChange1 = (event) => {
@@ -17,28 +18,6 @@ function Complainform() {
       [event.target.name]: event.target.value,
     });
   };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      
-      const db = firestore;
-
-      await addDoc(collection(db, 'forms'), formData);
-
-      console.log('Form data added to Firestore');
-
-      setFormData({
-        title: '',
-        location: '',
-        desc: '',
-      });
-    } catch (error) {
-      console.error('Error adding form data to Firestore:', error);
-    }
-  };
-
 
   // State to store uploaded file
   const [file, setFile] = useState("");
@@ -51,40 +30,63 @@ function Complainform() {
     setFile(event.target.files[0]);
   }
 
-  const handleUpload = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
     if (!file) {
       alert("Please upload an image first!");
+      return;
     }
-
+  
     const storageRef = ref(storage, `/images/${file.name}`);
-
-    // progress can be paused and resumed. It also exposes progress updates.
-    // Receives the storage reference and the file to upload.
     const uploadTask = uploadBytesResumable(storageRef, file);
-
+  
     uploadTask.on(
       "state_changed",
       (snapshot) => {
         const percent = Math.round(
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100
         );
-
+  
         // update progress
         setPercent(percent);
       },
       (err) => console.log(err),
-      () => {
-        // download url
-        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-          console.log(url);
-        });
+      async () => {
+        try {
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+          console.log(downloadURL);
+  
+          const updatedFormData = {
+            ...formData,
+            photoUrl: downloadURL
+          };
+  
+          const db = firestore;
+          await addDoc(collection(db, 'forms'), updatedFormData);
+          console.log('Form data added to Firestore');
+  
+          setFormData({
+            title: '',
+            location: '',
+            desc: '',
+            photoUrl: ''
+          });
+        } catch (error) {
+          console.error('Error adding form data to Firestore:', error);
+        }
       }
     );
   };
+  
+
+  
+
+
 
   return (
     <div className="bodyz">
-      
+
 
       <form className="form1" onSubmit={handleSubmit}>
         <label htmlFor="title">Title</label>
@@ -112,11 +114,11 @@ function Complainform() {
           onChange={handleChange1}
           value={formData.desc}
         />
-        
-        <input type="file" onChange={handleChange} accept="/image/*" />
+
+        <input type="file" onChange={handleChange} accept="image/*" />
         <>{percent} % done</>
 
-        <button type="submit" onClick={handleUpload}>Submit</button>
+        <button type="submit">Submit</button>
       </form>
       <div className="sidedesign">
         <h1>"The vote is not only a right; it is also a power." </h1>
