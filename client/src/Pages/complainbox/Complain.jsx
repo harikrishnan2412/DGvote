4f8457a640b7d21f7e98ed4ef4790c736b76a163
joel-roshan <joeljosephroshan@gmail.com>
2023-06-17 08:59:15 +0530

    modal

--- client/src/Pages/complainbox/Complain.jsx
+++ client/src/Pages/complainbox/Complain.jsx
@@ -13,13 +13,13 @@ const Complain = () => {
           img="https://images.pexels.com/photos/459728/pexels-photo-459728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         />
         <Complainbox
-          desc="joel is from kundara"
+          desc="kmgvkjsnfojknbjosdnbosfjnbopsf"
           location="Kannur"
           title="Poor Roads"
           img="https://images.pexels.com/photos/459728/pexels-photo-459728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         />
         <Complainbox
-          desc="joel is from kundara"
+          desc="mvgosdmvosngbsofjnbosfnoadn"
           location="Kannur"
           title="Poor Roads"
           img="https://images.pexels.com/photos/459728/pexels-photo-459728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
--- client/src/components/Complainbox/Complainbox.jsx
+++ client/src/components/Complainbox/Complainbox.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import "./Complainbox.css";
+import { useState } from "react";
 function Complainbox(props) {
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+      setModal(!modal);
+    };
   return (
-    <div className="complaint-card">
+    <>
+     {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-box">
+            <div className="modal-content"></div>
+                {props.desc}
+            </div>
+            </div>
+            )}
+
+
+
+    <div onClick={toggleModal} className="complaint-card">
       <div className="img-complain">
         <img src={props.img} alt="" loading="lazy" />
         <h2>{props.title}</h2>
         <p>{props.location}</p>
       </div>
     </div>
+    </>
+
   );
 }
 
